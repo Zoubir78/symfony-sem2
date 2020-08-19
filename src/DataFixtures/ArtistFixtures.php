505de778e5d00c50e7d68111b50dbae6e95024c8
Zoubir <marouf.zoubeir@gmail.com>
2020-08-19 20:18:36 +0200

    AJOUTE la liste des artistes

--- src/DataFixtures/ArtistFixtures.php
+++ src/DataFixtures/ArtistFixtures.php
@@ -9,11 +9,25 @@ class ArtistFixtures extends BaseFixture
     protected function loadData()
     {
         //Créer 50 artistes
-        $this->createMany(50, function(){
+        $this->createMany(50, 'artist',  function(){
+            // Construction du nom d'artiste
             return (new Artist())
                 ->setName($this->faker->name)
                 ->setDescription($this->faker->optional()->realText())
             ;    
         });
+
+        //  // Instanciation de l'entité
+        //  $artist = (new Artist())
+        //     ->setName($name)
+        //     ->setDescription($this->faker->realText(50))
+        // ;
+
+        //     // Retourner l'entité
+        //     return $artist;
+        // });
+
+        // // Enregistrer les entités en BDD
+        // $manager->flush();
     }
 }
--- src/DataFixtures/BaseFixtures.php
+++ src/DataFixtures/BaseFixtures.php
@@ -17,6 +17,8 @@ abstract class BaseFixture extends Fixture
     private $manager;
     /** @var Generator */
     protected $faker;
+    /** Liste des références aux entités générées par les fixtures */
+    private $references = [];
 
     /**
      * Méthode à implémenter par les classes qui héritent
@@ -43,12 +45,55 @@ abstract class BaseFixture extends Fixture
     /**
      * Enregistrer plusieurs entités
      * @param int $count           nombre d'entités à générer
+     * @param string $groupName    nom associé aux entités générées
      * @param callable $factory    fonction qui génère 1 entité
      */
 
-    protected function createMany(int $count, callable $factory)
+    protected function createMany(int $count, string $groupName, callable $factory)
     {
+        // Exécuter $factory $count fois
+        for ($i = 0; $i < $count; $i++) {
+            // La $factory doit retourner l'entité créée
+            $entity = $factory($i);
 
+            if ($entity === null) {
+                throw new \LogicException('Tu as oublié de retourner l\'entité !!!');
+            }
+
+            // Avertir Doctrine pour l'enregistrement de l'entité
+            $this->manager->persist($entity);
+
+            // Ajouter une référence pour l'entité
+            $this->addReference(sprintf('%s_%d', $groupName, $i), $entity);
+        }
+
+    }
+
+    /**
+     * Obtenir une entité aléatoire d'un groupe
+     */
+    protected function getRandomReference(string $groupName)
+    {
+        // Si les references ne sont pas présentes dans la propriété:
+        if (!isset($this->references[$groupName])) {
+            $this->references[$groupName] = [];
+
+            // Récupération des références
+            foreach ($this->referenceRepository->getReferences() as $key => $ref) {
+                if (strpos($key, $groupName . '_') === 0) {
+                    $this->references[$groupName][] = $key;
+                }
+            }
+        }
+
+          // Vérifier que des références ont été enregistrées
+          if (empty($this->references[$groupName])) {
+            throw new \Exception(sprintf('Aucune référence trouvée pour "%s"', $groupName));
+        }
+
+        // Retourner une référence aléatoire
+        $randomReferenceKey = $this->faker->randomElement($this->references[$groupName]);
+        return $this->getReference($randomReferenceKey);
     }
 }
 
